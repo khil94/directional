a5e75745d18c856a1cd387fc0c3f69734e3bf233
khil94 <zidru94@gmail.com>
2025-11-13 12:24:13 +0900

    refact : userStore 수정
    - persist 삭제
    - token 삭제
    - 토큰을 httpOnly 쿠키에서 관리하기로 함

--- app/store/user/userStore.ts
+++ app/store/user/userStore.ts
@@ -1,15 +1,13 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 export const USER_STORE_KEY = "directional-user-data";
 
 export interface UserState {
   isLoggedIn: boolean;
-  token: string | null;
 }
 
 interface UserAction {
-  login: (val: string) => void;
+  login: () => void;
   logout: () => void;
 }
 
@@ -17,22 +15,14 @@ export type UserStoreState = UserAction & UserState;
 
 const initState: UserState = {
   isLoggedIn: false,
-  token: null,
 };
 
-export const useUserStore = create(
-  persist<UserStoreState>(
-    (set, get) => ({
-      ...initState,
-      login: (token: string) => {
-        set({ isLoggedIn: true, token });
-      },
-      logout: () => {
-        set({ isLoggedIn: false, token: null });
-      },
-    }),
-    {
-      name: USER_STORE_KEY,
-    }
-  )
-);
+export const useUserStore = create<UserStoreState>((set, get) => ({
+  ...initState,
+  login: () => {
+    set({ isLoggedIn: true });
+  },
+  logout: () => {
+    set({ isLoggedIn: false });
+  },
+}));
