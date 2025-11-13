5b123adbae30eef86c2e771aea17dc58fcd630ae
khil94 <zidru94@gmail.com>
2025-11-13 18:23:33 +0900

    style : posts 테이블화

--- app/(private)/posts/page.tsx
+++ app/(private)/posts/page.tsx
@@ -1,5 +1,13 @@
 "use client";
 
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { usePosts } from "@/hooks/usePosts";
 import { Cursor, PostsRequest } from "@/types/post";
 import { useState } from "react";
@@ -45,11 +53,36 @@ export default function PostsPage() {
 
   return (
     <div>
-      <div>
-        {data?.items?.map((v) => {
-          return <div key={v.id}>{v.title}</div>;
-        })}
-      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>제목</TableHead>
+            <TableHead>카테고리</TableHead>
+            <TableHead>태그</TableHead>
+            <TableHead>생성 시각</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {data?.items?.map((v) => {
+            return (
+              <TableRow key={v.id}>
+                <TableCell>{v.title}</TableCell>
+                <TableCell>{v.category}</TableCell>
+                <TableCell>
+                  <div>
+                    {v.tags.map((t) => (
+                      <span key={`${v.id}-tag-${t}`}>{t}</span>
+                    ))}
+                  </div>
+                </TableCell>
+                <TableCell>
+                  {new Date(v.createdAt).toLocaleDateString()}
+                </TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
     </div>
   );
 }
