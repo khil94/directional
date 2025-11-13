b08b9fc7cba6e5ac40fd5d5fb402959e9fb79b5e
khil94 <zidru94@gmail.com>
2025-11-13 18:17:01 +0900

    feat : posts 페이지 1차 구현
    - 데이터만 불러오고 있다.
    - 사용할 예정인 함수들 미리 제작
      - cursor 관련 함수
      - filter updater

--- app/(private)/posts/page.tsx
+++ app/(private)/posts/page.tsx
@@ -1,4 +1,55 @@
+"use client";
+
+import { usePosts } from "@/hooks/usePosts";
+import { Cursor, PostsRequest } from "@/types/post";
+import { useState } from "react";
+
 export default function PostsPage() {
-  // const boardData = await API
-  return <div></div>;
+  const [params, setParams] = useState<PostsRequest>({
+    limit: 11,
+    search: "",
+  });
+
+  const [cursor, setCursor] = useState<Cursor>(undefined);
+
+  const { isLoading, isError, data } = usePosts(params, cursor);
+
+  function goNext(cursor: string) {
+    setCursor({
+      nextCursor: cursor,
+      prevCursor: undefined,
+    });
+  }
+
+  function goPrev(cursor: string) {
+    setCursor({
+      prevCursor: cursor,
+      nextCursor: undefined,
+    });
+  }
+
+  function resetCursor() {
+    setCursor(undefined);
+  }
+
+  function updateFilter(val: Partial<PostsRequest>) {
+    setParams((prev) => ({
+      ...prev,
+      ...val,
+      nextCursor: undefined,
+      prevCursor: undefined,
+    }));
+    resetCursor();
+  }
+  console.log("wtf", data);
+
+  return (
+    <div>
+      <div>
+        {data?.items?.map((v) => {
+          return <div key={v.id}>{v.title}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
