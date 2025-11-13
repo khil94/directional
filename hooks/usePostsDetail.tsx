edb1cc28451ae949d3b7b015ac25105bc3e4a68f
khil94 <zidru94@gmail.com>
2025-11-13 20:44:13 +0900

    fix : 누락 파일 usePostDetail

---
+++ hooks/usePostsDetail.tsx
@@ -0,0 +1,11 @@
+import { API } from "@/app/api/clientAPI";
+import { PostItem } from "@/types/post";
+import { useQuery } from "@tanstack/react-query";
+
+export function usePostDetail(id: string) {
+  return useQuery<PostItem>({
+    queryKey: ["postDetail", id],
+    queryFn: async () => await API.getPostDetail(id),
+    placeholderData: (prev) => prev,
+  });
+}
