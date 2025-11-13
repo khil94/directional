cf24bb64a44d3e7df2abf898c3846f2048d43b72
khil94 <zidru94@gmail.com>
2025-11-13 14:20:45 +0900

    fix : 폴더변경 미반영 된 부분 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Layout from "../layout/layout";
 import "./globals.css";
-import Layout from "./layout/layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
