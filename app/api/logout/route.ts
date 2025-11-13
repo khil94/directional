eccd5f2e7ab6bd77f2f1db2f6bc7130e9520a755
khil94 <zidru94@gmail.com>
2025-11-14 04:08:08 +0900

    refact : logout api 제대로

--- app/api/logout/route.ts
+++ app/api/logout/route.ts
@@ -1,9 +1,8 @@
+import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST() {
-  const response = NextResponse.json({ ok: true });
+  (await cookies()).delete("token");
 
-  response.cookies.delete("token");
-
-  return response;
+  return NextResponse.json({ ok: true });
 }
