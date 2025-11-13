b325929c6b527d9b9fa64bc9d3d43878ba1dc843
khil94 <zidru94@gmail.com>
2025-11-13 18:03:48 +0900

    refact : authAPI 또또 수정
    - cookies 부분은 서버 환경에서만 동작할 수 있다.
    - 따라서 client componet에서 호출하면 오류가 발생
    - 서버 로직은 완전히 분리
    - 토큰 가져와서 route가 자체적으로 넣도록 수정

--- app/api/lib/authApi.ts
+++ app/api/lib/authApi.ts
@@ -1,29 +1,14 @@
 import { cookies } from "next/headers";
-import { notFound } from "next/navigation";
 
-export async function AuthApi<T>(
-  input: string | URL | Request,
-  init?: RequestInit
-): Promise<T> {
+export async function getTokenHeader<T>(): Promise<Record<string, string>> {
   const cookiesStore = await cookies();
   const token = cookiesStore.get("token")?.value;
 
   if (!token) {
     throw new Error("인증 필요 (토큰 없음)");
   }
-  const res = await fetch(input, {
-    ...init,
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
 
-  if (!res.ok) {
-    if (res.status === 404) {
-      notFound();
-    }
-    throw new Error(`API ERROR by common api : ${res.status}`);
-  }
-
-  return res.json();
+  return {
+    Authorization: `Bearer ${token}`,
+  };
 }
